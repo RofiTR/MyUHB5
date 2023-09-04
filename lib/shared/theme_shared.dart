edab34440002c9ecfa5acbc1999802d04ed02aab
Rofi T <rofitaufiqurrohman@gmail.com>
2023-09-04 10:32:15 +0700

    selesai vidio 1

---
+++ lib/shared/theme_shared.dart
@@ -0,0 +1,12 @@
+part of 'shared.dart';
+
+double defaultmargin = 24; 
+
+Color primaryColor = Color(0xFF050522);
+Color secondaryColor = Color(0xFF050522);
+Color dangerColor = Color(0xFF050522);
+Color blackColor = Color(0xFF050522);
+Color whiteColor = Color(0xFF050522);
+
+TextStyle dangerTextStyle = GoogleFonts.roboto(fontSize:36, color: dangerColor, fontWeight: FontWeight.w500);
+TextStyle whoiteTextStyle = GoogleFonts.roboto(fontSize:36, color: whiteColor, fontWeight: FontWeight.w300);
